461a4ab5007fe1d7597f13ef6c86919fffccb2c9
ACOozeerally <adil.oozeerally@hotmail.com>
2018-12-10 12:21:26 +0000

    "Testing finished on Domain and Service"

--- src/test/java/com/ACO/test/domain/CvTest.java
+++ src/test/java/com/ACO/test/domain/CvTest.java
@@ -1,6 +1,7 @@
 package com.ACO.test.domain;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,24 +11,23 @@ import org.mockito.Mockito;
 import com.ACO.domain.CV;
 
 public class CvTest {
-	
+
 	private CV cv;
-	
+
 	@Before
 	public void init() {
 		cv = Mockito.mock(CV.class);
 	}
-	
-	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
 	public void domainTest() {
-		
+		String path = "Test Path";
+
+		cv.setCvPath(path);
+
+		when(cv.getCvPath()).thenReturn("Test Path");
+
+		assertEquals(path, cv.getCvPath());
 	}
 
 }
